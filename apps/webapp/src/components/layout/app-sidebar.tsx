532ceb8c529b96fa271fc64f3133431beb2a9bc1
Mikiyas Amdu <mikias.amdu@gmail.com>
2025-03-02 21:44:14 -0600

    feat: setup ui components

---
+++ apps/webapp/src/components/layout/app-sidebar.tsx
@@ -0,0 +1,30 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@webapp/components/ui/sidebar"
+import { NavGroup } from "@webapp/components/layout/nav-group"
+import { NavUser } from "@webapp/components/layout/nav-user"
+import { TeamSwitcher } from "@webapp/components/layout/team-switcher"
+import { sidebarData } from "./data/sidebar-data"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" variant="floating" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={sidebarData.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        {sidebarData.navGroups.map((props) => (
+          <NavGroup key={props.title} {...props} />
+        ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={sidebarData.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
